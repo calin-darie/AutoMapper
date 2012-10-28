4509fb6d2b643e5b379973017b2e8776049ada6e
Calin Darie <calin_darie@yahoo.com>
2012-10-28 08:24:09 +0200

    Adding author's test for #110; appears to be fixed

---
+++ src/UnitTests/MappingToPropertyWithoutGetter.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Should;
+using NUnit.Framework.SyntaxHelpers;
+namespace AutoMapper.UnitTests
+{
+    namespace MappingToPropertyWithoutGetter
+    {
+        [TestFixture]
+        public class MappingToPropertyWithoutGetter
+        {
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                Mapper.CreateMap<Source, Target>();
+            }
+
+            [Test]
+            public void Should_Not_Throw()
+            {
+                var source = new Source { Property = "foo bar" };
+                var target = Mapper.Map<Source, Target>(source);
+                Assert.That(target.GetProperty(), Is.EqualTo(source.Property));
+            }
+        }
+
+        public class Source
+        {
+            public string Property { get; set; }
+        }
+
+        public class Target
+        {
+            private string property;
+            public string Property
+            {
+                set { property = value; }
+            }
+
+            public string GetProperty()
+            {
+                return property;
+            }
+        }
+
+    }
+}
