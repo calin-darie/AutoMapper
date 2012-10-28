0f72ce8d8391e9fd88fb5b2676fbec0870c76f18
Calin Darie <calin_darie@yahoo.com>
2012-10-28 07:38:34 +0200

    Adding test for #187

---
+++ src/UnitTests/AssertToIgnorePrivateDestSettersFeatureRequest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using AutoMapper;
+using Should;
+namespace AutoMapper.UnitTests
+{
+
+    namespace AssertToIgnorePrivateDestSettersFeatureRequest
+    {
+        public class Foo { public int x { get; set; } }
+        public class Bar { public int x { get; private set; } }
+
+        [TestFixture]
+        public class WhenMappingToPropertyWithPrivateSetter
+        {
+            [SetUp]
+            public void Setup()
+            {
+                Mapper.CreateMap<Foo, Bar>();
+            }
+
+            [Test]
+            public void ShouldIgnoreProperty()
+            {
+                typeof(AutoMapperConfigurationException).ShouldNotBeThrownBy(Mapper.AssertConfigurationIsValid);
+            }
+        }
+    }
+}
